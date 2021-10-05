bdbf9aab94e0072dd1083ebfc6a4ca54f2c17ab1
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-10-05 16:18:43 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+  SELECT 
+    MIN(pl.valor) AS 'faturamento_minimo', 
+    MAX(pl.valor) AS 'faturamento_maximo',
+    ROUND(AVG(pl.valor), 2) AS 'faturamento_medio',
+    SUM(pl.valor) AS 'faturamento_total'
+  FROM planos AS pl
+  JOIN usuarios AS usr ON usr.plano_id = pl.plano_id;
